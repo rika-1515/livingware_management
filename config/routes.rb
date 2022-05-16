dea723450596e7d458d3b1838ed1644768f5e79f
EC2 Default User <ec2-user@ip-172-31-2-114.ec2.internal>
2022-05-16 14:45:46 +0000

    Add_デバイスインストール,コントローラ・モデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  
+  
+  
+  devise_for :customers, controllers: {
+    sessions: 'public/sessions',
+    registrations: 'public/registrations' # signup view -> app/controllers/public/registrations_controller.rb
+  }
+  
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions',
+    registrations: 'admin/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
