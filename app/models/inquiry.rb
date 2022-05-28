d12df2382cc71735452dc62469eec9fda5d9b983
EC2 Default User <ec2-user@ip-172-31-2-114.ec2.internal>
2022-05-28 00:33:19 +0000

    Add_会員問い合わせ送信機能

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -2,5 +2,6 @@ class Inquiry < ApplicationRecord
     belongs_to :customer
     
     enum support_status: { waiting: 0, working: 1,completed: 2 }
-    
+    validates :title, presence: true
+    validates :body, presence: true
 end
