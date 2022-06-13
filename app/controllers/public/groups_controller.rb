2b8cf6ba365078edee515c1140e42525c2ea8ba2
rika-1515 <rika.0322ex@gmail.com>
2022-06-13 01:46:27 +0000

    Add_日本時間設定、管理側会員編集修正

--- app/controllers/public/groups_controller.rb
+++ app/controllers/public/groups_controller.rb
@@ -1,10 +1,10 @@
 class Public::GroupsController < ApplicationController
   before_action :authenticate_customer!, except: [:top]
-  
+
   def edit
     @group = Group.find(current_customer.group_id)
   end
-  
+
   def update
     @group = Group.find(current_customer.group_id)
     if @group.update(group_params)
@@ -18,7 +18,7 @@ class Public::GroupsController < ApplicationController
     @group = current_customer.group
     @members = @group.customers
   end
-  
+
   def search
     @group = Group.where(token: params[:token]).first
     unless params[:commit]
@@ -26,13 +26,13 @@ class Public::GroupsController < ApplicationController
     end
     @customer = Customer.find(current_customer.id)
   end
-  
+
   def join
     @customer = Customer.find(current_customer.id)
     @customer.update(group_id_params)
     redirect_to groups_path
   end
-  
+
   def quit
     customer = Customer.find(current_customer.id)
     group = Group.find_by(group_leader_customer_id: customer.id)
@@ -40,7 +40,7 @@ class Public::GroupsController < ApplicationController
     customer.save
     redirect_to groups_path
   end
-  
+
   def out
     customer_id = params[:customer][:customer_id]
     group = Group.find_by(group_leader_customer_id: customer_id)
@@ -49,17 +49,17 @@ class Public::GroupsController < ApplicationController
     customer.save
     redirect_to groups_path
   end
-  
-  
-  
+
+
+
   private
   # ストロングパラメータ
   def group_params
     params.require(:group).permit(:name)
   end
-  
+
   def group_id_params
     params.require(:customer).permit(:group_id)
   end
-  
+
 end
--- config/application.rb
+++ config/application.rb
@@ -18,9 +18,10 @@ module LivingwareManagement
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    
+
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
-  
+    config.time_zone = 'Asia/Tokyo'
+
   end
 end
